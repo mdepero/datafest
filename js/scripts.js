b7ad8a0925cb56ebccab8f51e9603d32dd6674cb
Matt DePero <deperomm@miamioh.edu>
2017-04-08 17:16:06 -0400

    building site

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,22 @@ var something = 10;
 
 function start() {
 	new Morris.Line({
-	  element: 'test-chart',
+	  element: 'test-chart-1',
 	  data: [
 	    { y: 1, a: 0 },
 	    { y: 2, a: 2 },
 	    { y: 3, a: 5 },
-	    { y: 4, a: 15 },
-	    { y: 5, a: 7 },
-	    { y: 6, a: 2 },
-	    { y: 7, a: 1 }
+	    { y: 4, a: 10 },
+	    { y: 5, a: 20 },
+	    { y: 6, a: 40 },
+	    { y: 7, a: 80 },
+	    { y: 8, a: 40 },
+	    { y: 9, a: 10 },
+	    { y: 10, a: 5 },
+	    { y: 11, a: 3 },
+	    { y: 12, a: 2 },
+	    { y: 13, a: 1 },
+	    { y: 14, a: 1 }
 	  ],
 	  xkey: 'y',
 	  ykeys: ['a'],
@@ -26,7 +33,92 @@ function start() {
 	  events: [5],
 	  eventStrokeWidth: 4
 	});
+
+	new Morris.Line({
+	  element: 'test-chart-2',
+	  data: [
+	    { y: 1, a: 0 },
+	    { y: 2, a: 2 },
+	    { y: 3, a: 5 },
+	    { y: 4, a: 10 },
+	    { y: 5, a: 20 },
+	    { y: 6, a: 40 },
+	    { y: 7, a: 80 },
+	    { y: 8, a: 40 },
+	    { y: 9, a: 10 },
+	    { y: 10, a: 5 },
+	    { y: 11, a: 3 },
+	    { y: 12, a: 2 },
+	    { y: 13, a: 1 },
+	    { y: 14, a: 1 }
+	  ],
+	  xkey: 'y',
+	  ykeys: ['a'],
+	  labels: ['Series A', 'Series B'],
+	  resize: true,
+	  axes: false,
+	  hideHover: 'always',
+	  events: [3],
+	  eventStrokeWidth: 4
+	});
+
+	new Morris.Line({
+	  element: 'test-chart-3',
+	  data: [
+	    { y: 1, a: 0 },
+	    { y: 2, a: 2 },
+	    { y: 3, a: 5 },
+	    { y: 4, a: 10 },
+	    { y: 5, a: 20 },
+	    { y: 6, a: 40 },
+	    { y: 7, a: 80 },
+	    { y: 8, a: 40 },
+	    { y: 9, a: 10 },
+	    { y: 10, a: 5 },
+	    { y: 11, a: 3 },
+	    { y: 12, a: 2 },
+	    { y: 13, a: 1 },
+	    { y: 14, a: 1 }
+	  ],
+	  xkey: 'y',
+	  ykeys: ['a'],
+	  labels: ['Series A', 'Series B'],
+	  resize: true,
+	  axes: false,
+	  hideHover: 'always',
+	  events: [12],
+	  eventStrokeWidth: 4
+	});
+
+	new Morris.Line({
+	  element: 'test-chart-4',
+	  data: [
+	    { y: 1, a: 0 },
+	    { y: 2, a: 2 },
+	    { y: 3, a: 5 },
+	    { y: 4, a: 10 },
+	    { y: 5, a: 20 },
+	    { y: 6, a: 40 },
+	    { y: 7, a: 80 },
+	    { y: 8, a: 40 },
+	    { y: 9, a: 10 },
+	    { y: 10, a: 5 },
+	    { y: 11, a: 3 },
+	    { y: 12, a: 2 },
+	    { y: 13, a: 1 },
+	    { y: 14, a: 1 }
+	  ],
+	  xkey: 'y',
+	  ykeys: ['a'],
+	  labels: ['Series A', 'Series B'],
+	  resize: true,
+	  axes: false,
+	  hideHover: 'always',
+	  events: [10],
+	  eventStrokeWidth: 4
+	});
 }
+
 
 
 $(document).ready(start);
