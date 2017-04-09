ff8f74956b84bb2207fdababa6b8ccd985a54ef6
Matt DePero <deperomm@miamioh.edu>
2017-04-08 23:54:06 -0400

    it works!

--- js/scripts.js
+++ js/scripts.js
@@ -74,19 +74,72 @@ function dataReturnCallback(data) {
 
 	rawData = JSON.parse(data);
 
-	$('#test-chart-1-container').html('<div id="test-chart-1-percent"></div><div class="chart" id="test-chart-1"></div>');
+	if(rawData.setValues.branded != "1") $('#branded').html("You are NOT branded.");
+	else $('#branded').html("You are branded.");
 
-	$('#test-chart-1-percent').html(rawData.target/50);
+	if(rawData.setValues.rating == "0") $('#rating').html("You are not rated.");
+	else $('#ratings').html("Your rating is "+rawData.setValues.rating+".");
+
+	switch(rawData.setValues.popularity){
+		case "VL": 
+			$('#popularity').html("Your popularity is Very Low.");
+			break;
+		case "L": 
+			$('#popularity').html("Your popularity is Low.");
+			break;
+		case "M": 
+			$('#popularity').html("Your popularity is Medium.");
+			break;
+		case "H": 
+			$('#popularity').html("Your popularity is High.");
+			break;
+		case "VH": 
+			$('#popularity').html("Your popularity is Very High.");
+			break;
+	}
+
+	$('#test-chart-1-container').html('<div id="test-chart-1-percent"></div><div class="chart" id="test-chart-1"></div>');
+	$('#test-chart-1-percent').html(rawData.dataSets[1].target/40);
 
 	new Morris.Line({
 	  element: 'test-chart-1',
-	  data: rawData.data,
+	  data: rawData.dataSets[1].data,
 	  xkey: 'x',
 	  ykeys: ['y'],
 	  resize: true,
 	  axes: false,
 	  hideHover: 'always',
-	  events: [rawData.target],
+	  events: [rawData.dataSets[1].target],
+	  eventStrokeWidth: 4
+	});
+
+	$('#test-chart-2-container').html('<div id="test-chart-2-percent"></div><div class="chart" id="test-chart-2"></div>');
+	$('#test-chart-2-percent').html(rawData.dataSets[2].target/40);
+
+	new Morris.Line({
+	  element: 'test-chart-2',
+	  data: rawData.dataSets[2].data,
+	  xkey: 'x',
+	  ykeys: ['y'],
+	  resize: true,
+	  axes: false,
+	  hideHover: 'always',
+	  events: [rawData.dataSets[2].target],
+	  eventStrokeWidth: 4
+	});
+
+	$('#test-chart-3-container').html('<div id="test-chart-3-percent"></div><div class="chart" id="test-chart-3"></div>');
+	$('#test-chart-3-percent').html(rawData.dataSets[0].target/40);
+
+	new Morris.Line({
+	  element: 'test-chart-3',
+	  data: rawData.dataSets[0].data,
+	  xkey: 'x',
+	  ykeys: ['y'],
+	  resize: true,
+	  axes: false,
+	  hideHover: 'always',
+	  events: [rawData.dataSets[0].target],
 	  eventStrokeWidth: 4
 	});
 
