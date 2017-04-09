e6f542ffd1ebd7a38ad418143473c9c9c36fd5cb
Matt DePero <deperomm@miamioh.edu>
2017-04-08 22:33:36 -0400

    it works!

--- js/scripts.js
+++ js/scripts.js
@@ -3,122 +3,95 @@
  * DataFest 2017 - Miami University
  */
 
-var something = 10;
-
-function start() {
-	new Morris.Line({
-	  element: 'test-chart-1',
-	  data: [
-	    { y: 1, a: 0 },
-	    { y: 2, a: 2 },
-	    { y: 3, a: 5 },
-	    { y: 4, a: 10 },
-	    { y: 5, a: 20 },
-	    { y: 6, a: 40 },
-	    { y: 7, a: 80 },
-	    { y: 8, a: 40 },
-	    { y: 9, a: 10 },
-	    { y: 10, a: 5 },
-	    { y: 11, a: 3 },
-	    { y: 12, a: 2 },
-	    { y: 13, a: 1 },
-	    { y: 14, a: 1 }
-	  ],
-	  xkey: 'y',
-	  ykeys: ['a'],
-	  labels: ['Series A', 'Series B'],
-	  resize: true,
-	  axes: false,
-	  hideHover: 'always',
-	  events: [5],
-	  eventStrokeWidth: 4
-	});
-
-	new Morris.Line({
-	  element: 'test-chart-2',
-	  data: [
-	    { y: 1, a: 0 },
-	    { y: 2, a: 2 },
-	    { y: 3, a: 5 },
-	    { y: 4, a: 10 },
-	    { y: 5, a: 20 },
-	    { y: 6, a: 40 },
-	    { y: 7, a: 80 },
-	    { y: 8, a: 40 },
-	    { y: 9, a: 10 },
-	    { y: 10, a: 5 },
-	    { y: 11, a: 3 },
-	    { y: 12, a: 2 },
-	    { y: 13, a: 1 },
-	    { y: 14, a: 1 }
-	  ],
-	  xkey: 'y',
-	  ykeys: ['a'],
-	  labels: ['Series A', 'Series B'],
-	  resize: true,
-	  axes: false,
-	  hideHover: 'always',
-	  events: [3],
-	  eventStrokeWidth: 4
-	});
-
-	new Morris.Line({
-	  element: 'test-chart-3',
-	  data: [
-	    { y: 1, a: 0 },
-	    { y: 2, a: 2 },
-	    { y: 3, a: 5 },
-	    { y: 4, a: 10 },
-	    { y: 5, a: 20 },
-	    { y: 6, a: 40 },
-	    { y: 7, a: 80 },
-	    { y: 8, a: 40 },
-	    { y: 9, a: 10 },
-	    { y: 10, a: 5 },
-	    { y: 11, a: 3 },
-	    { y: 12, a: 2 },
-	    { y: 13, a: 1 },
-	    { y: 14, a: 1 }
-	  ],
-	  xkey: 'y',
-	  ykeys: ['a'],
-	  labels: ['Series A', 'Series B'],
-	  resize: true,
-	  axes: false,
-	  hideHover: 'always',
-	  events: [12],
-	  eventStrokeWidth: 4
-	});
-
-	new Morris.Line({
-	  element: 'test-chart-4',
-	  data: [
-	    { y: 1, a: 0 },
-	    { y: 2, a: 2 },
-	    { y: 3, a: 5 },
-	    { y: 4, a: 10 },
-	    { y: 5, a: 20 },
-	    { y: 6, a: 40 },
-	    { y: 7, a: 80 },
-	    { y: 8, a: 40 },
-	    { y: 9, a: 10 },
-	    { y: 10, a: 5 },
-	    { y: 11, a: 3 },
-	    { y: 12, a: 2 },
-	    { y: 13, a: 1 },
-	    { y: 14, a: 1 }
-	  ],
-	  xkey: 'y',
-	  ykeys: ['a'],
-	  labels: ['Series A', 'Series B'],
-	  resize: true,
-	  axes: false,
-	  hideHover: 'always',
-	  events: [10],
-	  eventStrokeWidth: 4
-	});
+// from https://css-tricks.com/snippets/javascript/get-url-variables/
+function getQueryVariable(variable) {
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
 
+
+
+
+var something = 10;
+
+var rawData;
+
+
+function start() {
+	if (getQueryVariable('id') == '' || getQueryVariable('id')===undefined) {
+		window.location = './start';
+	}else{
+		//getCurrentData();
+	}
+
+	$('#run').click(getCurrentData);
+}
+
+function getCurrentData() {
+	console.log("Sending request...");
+	var sql = "SELECT * FROM Clicks WHERE ";
+	var label = "";
+	var first = true;
+	$('input[type=checkbox]').each(function () {
+           if (this.checked) {
+                if(this.name != label) {
+                	if(first) first = false;
+                	else sql += ') AND ';
+                	sql += this.name + ' in (' + "'" + this.value + "'";
+
+                	label = this.name;
+                }else{
+                	sql += ','+ "'" + this.value + "'";
+                }
+           }
+	});
+	sql += ')';
+	console.log(sql);
+	$.post(
+		"http://localhost:8008/",
+	    {
+	        query: sql,
+	        id: getQueryVariable('id')
+	    },
+    	dataReturnCallback
+	);
+}
+
+function dataReturnCallbackTest(data) {
+	console.log(data);
+	//console.log(JSON.parse(data));
+}
+
+
+function dataReturnCallback(data) {
+
+	console.log(data);
+
+	rawData = JSON.parse(data);
+
+	$('#test-chart-1-container').html('<div id="test-chart-1-percent"></div><div class="chart" id="test-chart-1"></div>');
+
+	$('#test-chart-1-percent').html(rawData.target/50);
+
+	new Morris.Line({
+	  element: 'test-chart-1',
+	  data: rawData.data,
+	  xkey: 'x',
+	  ykeys: ['y'],
+	  resize: true,
+	  axes: false,
+	  hideHover: 'always',
+	  events: [rawData.target],
+	  eventStrokeWidth: 4
+	});
+
+	
+}
 
 
 $(document).ready(start);
